03c9628e620edde43d12e3b692f0f65a77f381f7
Minhazul Hasan Sohan <minhazul.hasan.sohan@gmail.com>
2020-11-19 04:51:27 +0600

    done some component initially

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,23 +1,24 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 import bg from '../../resources/16 [Converted]@2x.png';
 
 const Header = () => {
     return (
-        <div className='d-flex flex-wrap px-5 mt-5 ' style={{ overFlow: 'hidden' }}>
-            <div className='mt-5 col-md-4 d-flex align-items-center container'>
-                <div className='container container-fluid'>
-                    <h1 className='specialTexts header-text'>
-                        Florence  <br />
-                    Agency
-                </h1>
-                    <p className='text-muted'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt excepturi rem eos earum explicabo sunt voluptatum cupiditate odit sit laudantium, nobis optio quasi neque autem repellat aperiam a voluptas vel.</p>
-                    <button>See Pricing</button>
+        <Row className='py-5 container-fluid'>
+            <Col lg={{ span: 4, offset: 1 }} md={{ span: 4, offset: 1 }} sm={{ span: 10, offset: 1 }} className="d-flex align-items-center">
+                <div>
+                    <h1 className='header-text'>Florence <br /> agency</h1>
+                    <p style={{fontSize: '14px'}} className='py-4 text-muted'>Lorem Ipsum has been the industry's standard dummy text ever<br />
+                        since the 1500s, when an unknown printer took a galley of type and<br />
+                        scrambled it to make a type specimen book.</p>
+                    <button className="common-btn">See Pricing</button>
                 </div>
-            </div>
-            <div className='col-md-8 justify-content-end'>
-                <img className="img-fluid ml-5" style={{ height: '70vh' }} src={bg} alt="" />
-            </div>
-        </div>
+            </Col>
+            <Col lg={{ span: 5, offset: 1 }} md={{ span: 6, offset: 1 }} sm={{ span: 10, offset: 1 }}>
+                <img className="img-fluid" src={bg} alt="" />
+            </Col>
+            <Col lg={1} md={0}></Col>
+        </Row>
     );
 };
 
