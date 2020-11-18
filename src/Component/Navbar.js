07d8b0b9ef793827d06e53b53c0874b2f16e323a
Mohammad Fahad <mail.maohammadfahad@gmail.com>
2020-11-18 23:07:15 +0600

    test

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import logo from '../resources/Group 86.png';
 
 const Navbar = () => {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light container">
-                <a className="navbar-brand" href="#">Navbar</a>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <img src={logo} alt="" />
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -22,6 +23,7 @@ const Navbar = () => {
                         <li className="nav-item">
                             <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
                         </li>
+                        <button style={{ borderRadius: '30px 0' }} className="px-5 py-3">Contact Us</button>
                     </ul>
                 </div>
             </nav>
